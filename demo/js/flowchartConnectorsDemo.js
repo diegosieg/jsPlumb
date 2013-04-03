2c7a11fe7aa0419c9de110c16184702bdbb0fe20
Diego da Silva Pinto <diego.pinto@totvs.com.br>
2013-04-03 14:44:41 -0300

    layout improvements

--- demo/js/flowchartConnectorsDemo.js
+++ demo/js/flowchartConnectorsDemo.js
@@ -4,8 +4,8 @@
 		init : function() {
 			jsPlumb.importDefaults({
 				DragOptions : { cursor: 'pointer', zIndex:2000 },
-				EndpointStyles : [{ fillStyle:'#225588' }, { fillStyle:'#558822' }],
-				Endpoints : [ [ "Dot", {radius:7} ], [ "Dot", { radius:11 } ]],
+				EndpointStyles : [{ fillStyle:'#27AE60' }, { fillStyle:'#2980B9' }],
+				Endpoints : [ [ "Dot", {radius:7} ], [ "Dot", { radius:7 } ]],
 				ConnectionOverlays : [
 					[ "Arrow", { location:0.9 } ],
 					[ "Label", {
@@ -17,21 +17,21 @@
 			});
 
 			var connectorPaintStyle = {
-				lineWidth:5,
-				strokeStyle:"#deea18",
+				lineWidth:2,
+				strokeStyle:"#2ECC71",
 				joinstyle:"round",
 				outlineColor:"#EAEDEF",
 				outlineWidth:7
 			},
 
 			connectorHoverStyle = {
-				lineWidth:7,
-				strokeStyle:"#2e2aF8"
+				lineWidth:2,
+				strokeStyle:"#E74C3C"
 			},
 
 			sourceEndpoint = {
 				endpoint:"Dot",
-				paintStyle:{ fillStyle:"#225588",radius:7 },
+				paintStyle:{ fillStyle:"#2980B9",radius:7 },
 				isSource:true,
 				connector:[ "Flowchart", { stub:[40, 60], gap:10 } ],
 				connectorStyle:connectorPaintStyle,
@@ -49,7 +49,7 @@
 
 			targetEndpoint = {
 				endpoint:"Dot",
-				paintStyle:{ fillStyle:"#558822",radius:11 },
+				paintStyle:{ fillStyle:"#27AE60",radius:7 },
 				hoverPaintStyle:connectorHoverStyle,
 				maxConnections:-1,
 				dropOptions:{ hoverClass:"hover", activeClass:"active" },
@@ -78,12 +78,13 @@
 					}
 				};
 
-			_addEndpoints("window6", ["TopCenter", "TopLeft", "TopRight", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);
-			_addEndpoints("window5", ["TopCenter", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);
-			_addEndpoints("window4", ["TopCenter", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);
-			_addEndpoints("window2", ["LeftMiddle", "BottomCenter"], ["TopCenter", "RightMiddle"]);
-			_addEndpoints("window3", ["RightMiddle", "BottomCenter"], ["LeftMiddle", "TopCenter"]);
-			_addEndpoints("window1", ["LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter"]);
+			// _addEndpoints("window6", ["TopCenter", "TopLeft", "TopRight", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);
+			// _addEndpoints("window5", ["TopCenter", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);
+			// _addEndpoints("window4", ["TopCenter", "BottomCenter"], ["LeftMiddle", "RightMiddle"]);
+			// _addEndpoints("window2", ["LeftMiddle", "BottomCenter"], ["TopCenter", "RightMiddle"]);
+			// _addEndpoints("window3", ["RightMiddle", "BottomCenter"], ["LeftMiddle", "TopCenter"]);
+			// _addEndpoints("window1", ["LeftMiddle", "RightMiddle"], ["TopCenter", "BottomCenter"]);
+
 
 			jsPlumb.bind("jsPlumbConnection", function(connInfo, originalEvent) {
 				init(connInfo.connection);
@@ -92,16 +93,21 @@
 
 			jsPlumb.draggable(jsPlumb.getSelector(".window"), { grid: [1, 1] });
 
-			jsPlumb.connect({uuids:["window2BottomCenter", "window3TopCenter"], editable:true});
-			jsPlumb.connect({uuids:["window2LeftMiddle", "window4LeftMiddle"], editable:true});
-			jsPlumb.connect({uuids:["window4TopCenter", "window4RightMiddle"], editable:true});
-			jsPlumb.connect({uuids:["window3RightMiddle", "window2RightMiddle"], editable:true});
-			jsPlumb.connect({uuids:["window4BottomCenter", "window1TopCenter"], editable:true});
-			jsPlumb.connect({uuids:["window3BottomCenter", "window1BottomCenter"], editable:true});
+			// jsPlumb.connect({uuids:["window2BottomCenter", "window3TopCenter"], editable:true});
+			// jsPlumb.connect({uuids:["window2LeftMiddle", "window4LeftMiddle"], editable:true});
+			// jsPlumb.connect({uuids:["window4TopCenter", "window4RightMiddle"], editable:true});
+			// jsPlumb.connect({uuids:["window3RightMiddle", "window2RightMiddle"], editable:true});
+			// jsPlumb.connect({uuids:["window4BottomCenter", "window1TopCenter"], editable:true});
+			// jsPlumb.connect({uuids:["window3BottomCenter", "window1BottomCenter"], editable:true});
 
 			jsPlumb.bind("click", function(conn, originalEvent) {
 				if (confirm("Delete connection from " + conn.sourceId + " to " + conn.targetId + "?"))
 					jsPlumb.detach(conn);
+			});
+
+			jsPlumb.bind("dblclick", function(connInfo, originalEvent) {
+				if (confirm("Delete box " + connInfo.sourceId +"?"))
+					jsPlumb.detach(connInfo);
 			});
 
 			jsPlumb.bind("connectionDrag", function(connection) {
